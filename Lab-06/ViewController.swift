595f6f5b0d4fcc5d8205509f32aa626011b46d5b
Mac Tecsup <mactecsup@MacBook-Air-de-Mac.local>
2017-09-18 15:00:04 -0500

    Initial Commit

---
+++ Lab-06/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab-06
+//
+//  Created by Mac Tecsup on 18/09/17.
+//  Copyright © 2017 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
