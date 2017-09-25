54ab09d40c751d872c5cc04326b7b7b5de1afca5
Mac Tecsup <martin.chavez@tecsup.edu.pe>
2017-09-25 17:13:44 -0500

    commit3

--- Lab-06/ViewController.swift
+++ Lab-06/ViewController.swift
@@ -40,6 +40,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             
         }
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let juego = juegos[indexPath.row]
+        performSegue(withIdentifier: "juegoSegue", sender: juego)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let siguienteVC = segue.destination as! JuegoViewController
+        siguienteVC.juego = sender as? Juego
+    }
 
 }
 
